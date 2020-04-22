d137f959f326f1073e7752a5effd817ca3a92413
Michael Fearn <fearn.michael@gmail.com>
2020-04-21 19:18:13 -0600

    chapter 3

--- index.test.ts
+++ index.test.ts
@@ -8,4 +8,9 @@ describe("Dollar", () => {
     product = five.times(3);
     expect(product.amount).toBe(15);
   });
+
+  test("Equality", () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+  });
 });
