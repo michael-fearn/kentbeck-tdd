fb9c360c5bd408970d2ce20236557230c2250579
Michael Fearn <fearn.michael@gmail.com>
2020-04-22 17:43:00 -0600

    chapter 11

--- index.test.ts
+++ index.test.ts
@@ -20,11 +20,6 @@ describe("Money", () => {
   test("Equality", () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
-
-    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
-    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
-
     expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
-    expect(Money.dollar(5).equals(Money.franc(5))).toBeFalsy();
   });
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export abstract class Money {
+export class Money {
   protected amount: number;
   protected _currency: string;
   constructor(amount: number, currency: string) {
@@ -7,39 +7,21 @@ export abstract class Money {
   }
 
   static dollar(amount: number) {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
 
   static franc(amount: number) {
-    return new Franc(amount, "CHF");
+    return new Money(amount, "CHF");
+  }
+
+  public times(multiplier: number): Money {
+    return new Money(this.amount * multiplier, this.currency());
   }
 
   public equals(money: Money): boolean {
-    return (
-      this.amount == money.amount &&
-      money.constructor.name == this.constructor.name
-    );
+    return this.amount == money.amount && money.currency() == this.currency();
   }
   currency(): string {
     return this._currency;
-  }
-}
-
-export class Dollar extends Money {
-  constructor(amount: number, currency: string = "USD") {
-    super(amount, currency);
-  }
-
-  public times(multiplier: number): Money {
-    return Money.dollar(this.amount * multiplier);
-  }
-}
-
-export class Franc extends Money {
-  constructor(amount: number, currency: string = "CHF") {
-    super(amount, currency);
-  }
-  public times(multiplier: number): Money {
-    return Money.franc(this.amount * multiplier);
   }
 }
